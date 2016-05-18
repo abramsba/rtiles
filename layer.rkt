464ffdb7d6850069fe5880dc521c1b255066262f
Bryan Abrams <bryan.abrams.eu@gmail.com>
2016-05-18 20:41:35 +0200

    Layer zone split

---
+++ layer.rkt
@@ -0,0 +1,95 @@
+#lang racket
+
+(require 
+  racket/draw
+  "vec.rkt"
+  "chunk.rkt" 
+  "utils.rkt")
+
+(provide (all-defined-out) layer%)
+
+(define layer%
+  (class object%
+    (init-field id chunk zindex)
+    (inspect (make-inspector))
+    (field
+     [move-block #f]
+     [los-block #f]
+     [character #\ ]
+     [bg-color "black"]
+     [fg-color "white"]
+     [bg-alpha 1.0]
+     [fg-alpha 1.0]
+     [offset-x 0]
+     [offset-y 0])
+    (super-new)
+    (define/public (id?) id)
+    (define/public (chunk?) chunk)
+    (define/public (size?) (send chunk size?))
+    (define/public (move-block?) move-block)
+    (define/public (move-block! b)
+      (set! move-block b))
+    (define/public (los-block?) los-block)
+    (define/public (los-block! b)
+      (set! los-block b))
+    (define/public (zindex?) zindex)
+    (define/public (zindex! z)
+      (set! zindex z))
+    (define/public (character?) character)
+    (define/public (character! c)
+      (set! character c))
+    (define/public (locked?)
+      (let-values ([(cls skp) (object-info chunk)])
+        (equal? cls chunk-immutable%)))
+    (define/public (unlock)
+      (when (send this locked?)
+        (set! chunk (send chunk ->mutable))))
+    (define/public (lock)
+      (unless (send this locked?)
+        (set! chunk (send chunk ->immutable))))
+    (define/public (fg-color?) fg-color)
+    (define/public (fg-color! c)
+      (set! fg-color c))
+    (define/public (bg-color?) bg-color)
+    (define/public (bg-color! c)
+      (set! bg-color c))
+    (define/public (fg-alpha?) fg-alpha)
+    (define/public (fg-alpha! a)
+      (set! fg-alpha a))
+    (define/public (bg-alpha?) bg-alpha)
+    (define/public (bg-alpha! a)
+      (set! bg-alpha a))
+    (define/public (offset-x?) offset-x)
+    (define/public (offset-x! x)
+      (set! offset-x x))
+    (define/public (offset-y?) offset-y)
+    (define/public (offset-y! y)
+      (set! offset-y y))
+    (define/public (->bitmap ts)
+      (let*
+        ([sz (* ts (send chunk size?))]
+         [bitmap (make-bitmap sz sz)]
+         [dc (new bitmap-dc% [bitmap bitmap])]
+         [fnt (make-font #:size (/ ts 1.4) #:family 'roman)])
+        (send dc set-pen "black" 1 'transparent)
+        (send dc set-brush bg-color 'solid)
+        (send dc set-font fnt)
+        (for ([v (send chunk ->veclist)])
+          (let ([x (* ts (vec-x v))] [y (* ts (vec-y v))])
+            (send dc set-alpha bg-alpha)
+            (send dc draw-rectangle x y ts ts)
+            (send dc set-alpha fg-alpha)
+            (send dc set-text-foreground fg-color)
+            (send dc draw-text (string character) (+ x 2) y)))
+        bitmap))
+    (define/public (->jsexpr)
+      (hasheq 'id (symbol->string id)
+              'chunk (send chunk ->jsexpr)
+              'zindex zindex
+              'locked (send this locked?)
+              'move-block move-block
+              'los-block los-block
+              'bg-color bg-color
+              'fg-color fg-color
+              'offset-x offset-x
+              'offset-y offset-y))))
--- zone.rkt
+++ zone.rkt
@@ -1,77 +1,14 @@
 #lang racket
 
-(require "chunk.rkt" "utils.rkt")
+(require 
+  racket/draw
+  "vec.rkt"
+  "chunk.rkt"
+  "layer.rkt"
+  "utils.rkt")
 
-(provide (all-defined-out) zone-layer% zone%)
+(provide (all-defined-out) zone%)
 
-(define zone-layer%
-  (class object%
-    (init-field id chunk zindex)
-    (inspect (make-inspector))
-    (field
-     [move-block #f]
-     [los-block #f]
-     [character #\ ]
-     [bg-color "black"]
-     [fg-color "white"]
-     [bg-alpha 1.0]
-     [fg-alpha 1.0]
-     [offset-x 0]
-     [offset-y 0])
-    (super-new)
-    (define/public (id?) id)
-    (define/public (chunk?) chunk)
-    (define/public (move-block?) move-block)
-    (define/public (move-block! b)
-      (set! move-block b))
-    (define/public (los-block?) los-block)
-    (define/public (los-block! b)
-      (set! los-block b))
-    (define/public (zindex?) zindex)
-    (define/public (zindex! z)
-      (set! zindex z))
-    (define/public (character?) character)
-    (define/public (character! c)
-      (set! character c))
-    (define/public (locked?)
-      (let-values ([(cls skp) (object-info chunk)])
-        (equal? cls chunk-immutable%)))
-    (define/public (unlock)
-      (when (send this locked?)
-        (set! chunk (send chunk ->mutable))))
-    (define/public (lock)
-      (unless (send this locked?)
-        (set! chunk (send chunk ->immutable))))
-    (define/public (fg-color?) fg-color)
-    (define/public (fg-color! c)
-      (set! fg-color c))
-    (define/public (bg-color?) bg-color)
-    (define/public (bg-color! c)
-      (set! bg-color c))
-    (define/public (fg-alpha?) fg-alpha)
-    (define/public (fg-alpha! a)
-      (set! fg-alpha a))
-    (define/public (bg-alpha?) bg-alpha)
-    (define/public (bg-alpha! a)
-      (set! bg-alpha a))
-    (define/public (offset-x?) offset-x)
-    (define/public (offset-x! x)
-      (set! offset-x x))
-    (define/public (offset-y?) offset-y)
-    (define/public (offset-y! y)
-      (set! offset-y y))
-    (define/public (->jsexpr)
-      (hasheq 'id (symbol->string id)
-              'chunk (send chunk ->jsexpr)
-              'zindex zindex
-              'locked (send this locked?)
-              'move-block move-block
-              'los-block los-block
-              'bg-color bg-color
-              'fg-color fg-color
-              'offset-x offset-x
-              'offset-y offset-y))))
-      
 (define zone%
   (class object%
     (init-field id)
@@ -81,6 +18,8 @@
     (define/public (add-layer ly)
       (unless (eq? #t (send this has-layer? ly))
         (set! layers (append layers (list ly)))))
+    (define/public (size?)
+      (apply max (for/list ([l (send this ->list)]) (send l size?))))
     (define/public (has-layer? ly)
       (not (equal? #f (member ly layers))))
     (define/public (get-layer ly-id)
@@ -95,12 +34,25 @@
       (sort layers
             (lambda (x y)
               (< (send x zindex?) (send y zindex?)))))
+    (define/public (->bitmap ts)
+      (let*
+          ([sz (* (send this size?) ts)]
+           [bitmap (make-bitmap sz sz)]
+           [dc (new bitmap-dc% [bitmap bitmap])])
+        (for ([i (send this ->list)])
+          (send dc draw-bitmap (send i ->bitmap ts) 0 0))
+        bitmap))
     (define/public (->jsexpr)
       (let
           ([json_layers (for/list ([l layers]) (send l ->jsexpr))])
         (hasheq 'id (symbol->string id)
                 'layers json_layers)))))
-    
 
-      
+
+
+
+
+
+
+
 
